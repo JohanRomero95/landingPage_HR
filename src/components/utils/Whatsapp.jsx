c53c626dd3f851c69da13e75ac3c9b583dd2994c
JohanRomero95 <johanarm95@gmail.com>
2024-11-07 01:41:48 -0300

    fix

--- src/components/utils/Whatsapp.jsx
+++ src/components/utils/Whatsapp.jsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function WhatsAppButton() {
 	return (
-		<button
+		<a
 			href="https://wa.me/56982690613"
 			target="_blank"
 			rel="noopener noreferrer"
@@ -10,6 +10,6 @@ export default function WhatsAppButton() {
 			title="Botón ir al inicio"
 			className="fixed bottom-5 right-5 bg-[#25D366] w-14 h-14 rounded-full flex items-center justify-center shadow-lg hover:bg-[#1ebd59] transition duration-300">
 			<FaWhatsapp className="text-white text-3xl" />
-		</button>
+		</a>
 	);
 }
